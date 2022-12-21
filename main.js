9f704b57eab8ee793f1565907984a2492450b3ea
JayantiTA <jayjayanti18@gmail.com>
2022-12-21 10:26:24 +0700

    feat: add graphics contents

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { FlyControls } from 'three/addons/controls/FlyControls.js';
 import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 import * as CTRL from './assets/js/control.js';
 import * as AN from './assets/js/animation.js';
 import * as OM from './assets/js/object_maker.js';
@@ -39,7 +41,7 @@ function init() {
   // document.body.appendChild(container);
 
   camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 100);
-  camera.position.set(10, 5, 1000);
+  camera.position.set(0, 1, 4);
   cameraLookAt = new THREE.Vector3(0, 0, 0);
   camera.lookAt(cameraLookAt.x, cameraLookAt.y, cameraLookAt.z);
 
@@ -102,7 +104,6 @@ function init() {
     mesh.name = name;
 
     scene.add(mesh);
-
   }
 
   function handle_load_fbx(fbx, translation, rotation, scale, name) {
@@ -221,7 +222,7 @@ function init() {
 
   gltfLoader.load('models/properties/fountain.glb',
     function (gltf) {
-      handle_load_gltf(gltf, -1, [-2.6, 0.08, 1.2], [0, 0, 0], [0.1, 0.1, 0.1], mesh)
+      handle_load_gltf(gltf, -1, [-2.4, 0.14, 1.2], [0, 0, 0], [0.2, 0.2, 0.2], mesh)
     },
   );
 
@@ -377,7 +378,7 @@ function init() {
     },
   );
 
-  fbxLoader.load('models/vehicles/motodraft.fbx',
+  fbxLoader.load('models/vehicles/honda/source/motodraft.fbx',
     function (object) {
       handle_load_fbx(object, [3.1, 0.06, -0.2], [0, -3.14, 0], [0.001, 0.001, 0.001])
     },
@@ -427,6 +428,187 @@ function init() {
 
   let canvas = document.getElementById('canvas');
   renderer = new THREE.WebGLRenderer({ canvas: canvas});
+  const geometry3 = new THREE.CircleGeometry( 0.05, 32 );
+  const material3 = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+  const circle = new THREE.Mesh( geometry3, material3 );
+  circle.position.set(-0.55, 0.65, 1.3);
+  circle.rotation.set(0, -0.77, 0);
+  circle.name = "third_floor";
+  scene.add( circle );
+
+  const circle2 = new THREE.Mesh( geometry3, material3 );
+  circle2.position.set(-0.55, 0.4, 1.3);
+  circle2.rotation.set(0, -0.77, 0);
+  circle2.name = "second_floor";
+  scene.add( circle2 );
+
+  const circle3 = new THREE.Mesh( geometry3, material3 );
+  circle3.position.set(-0.55, 0.15, 1.3);
+  circle3.rotation.set(0, -0.77, 0);
+  circle3.name = "first_floor";
+  scene.add( circle3 );
+
+  const circle4 = new THREE.Mesh( geometry3, material3 );
+  circle4.position.set(-2, 0.6, -1.7);
+  circle4.name = "canteen";
+  scene.add( circle4 );
+
+  const raycaster = new THREE.Raycaster();
+  const mouse = new THREE.Vector2();
+
+  const close = new THREE.Mesh( geometry3, material3 );
+  close.position.set(0.6, 1.05, 1.3);
+  close.rotation.set(0, -0.65, 0);
+  close.visible = false;
+  close.name = "close";
+  scene.add(close);
+
+  const close2 = new THREE.Mesh( geometry3, material3 );
+  close2.position.set(-2, 0.95, -1.7);
+  close2.visible = false;
+  close2.name = "close2";
+  scene.add(close2);
+
+  const cubeGeometry = new THREE.BoxGeometry(0.4, 0.4, 0.01);
+  const cubeMaterial = new THREE.MeshBasicMaterial( { color: 0xbd8661 } );
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  cube.position.set(-0.25, 0.5, 1.46);
+  cube.rotation.set(0, -0.6, 0);
+  cube.visible = false;
+  scene.add(cube);
+
+  const cube2 = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  cube2.position.set(-2, 0.7, -1.7);
+  cube2.visible = false;
+  scene.add(cube2);
+
+  let font = undefined;
+  const text_first_floor = ["Lantai 1:", "Ruang Kelas Mahasiswa", "Plaza Supeno", "Musola", "PIKTI"];
+  const text_second_floor = ["Lantai 2:", "Ruang Dosen", "Ruang TU", "Aula Handayani"];
+  const text_third_floor = ["Lantai 3:", "Ruang Lab", "Sekre HMTC"];
+  const text_canteen = ["Menu:", "Ayam Geprek", "Nasi Goreng", "Mie Ayam", "Soto Ayam", "Sego Njamoer", "Minuman"];
+  const textVisible = [];
+
+  function loadFont(text, position, rotation) {
+    const loader = new FontLoader();
+    loader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function (res) {
+      font = res;
+      createText(text, position, rotation);
+    });
+  }
+
+  function createText(textArray, position, rotation) {
+    let intialY = position[1];
+    textVisible.splice(0, textVisible.length);
+    textArray.forEach((txt) => {
+      const textGeometry = new TextGeometry(txt, {
+        font: font,
+        size: 0.02,
+        height: 0.001,
+        bevelThickness: 0.01,
+        bevelSize: 0.001,
+        bevelEnabled: true,
+      });
+      textGeometry.computeBoundingBox();
+      const textMaterial = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+
+      const text = new THREE.Mesh(textGeometry, textMaterial);
+      text.position.set(position[0], intialY, position[2]);
+      text.rotation.set(rotation[0], rotation[1], rotation[2]);
+      intialY -= 0.05;
+      text.castShadow = true;
+      textVisible.push(text);
+      scene.add(text);
+    });
+  }
+
+  function onMouseMove( event ) {
+    // calculate mouse position in normalized device coordinates
+    // (-1 to +1) for both components
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  }
+
+  function onMouseClick( event ) {
+    // calculate objects intersecting the picking ray
+    raycaster.setFromCamera( mouse, camera );
+    const intersects = raycaster.intersectObjects(scene.children);
+  
+    for ( let i = 0; i < intersects.length; i++ ) {
+      if (intersects[i].object.name == 'first_floor') {
+        textVisible.forEach((txt) => {
+          txt.visible = false;
+        });
+        intersects[i].object.material.color.set( 0xff0000 );
+        window.location.hash = "first_floor";
+        camera.position.set(-1, 0.5, 2.5);
+        camera.lookAt(0, 0.5);
+        close.visible = true;
+        cube.visible = true;
+        loadFont(text_first_floor, [-0.39, 0.6, 1.37], [0, -0.6, 0]);
+      }
+      if (intersects[i].object.name == 'second_floor') {
+        textVisible.forEach((txt) => {
+          txt.visible = false;
+        });
+        intersects[i].object.material.color.set( 0x00ff00 );
+        window.location.hash = "second_floor";
+        camera.position.set(-1, 0.5, 2.5);
+        camera.lookAt(0, 0.5);
+        close.visible = true;
+        cube.visible = true;
+        loadFont(text_second_floor, [-0.39, 0.6, 1.37], [0, -0.6, 0]);
+      }
+      if (intersects[i].object.name == 'third_floor') {
+        textVisible.forEach((txt) => {
+          txt.visible = false;
+        });
+        intersects[i].object.material.color.set( 0x0000ff );
+        window.location.hash = "third_floor";
+        camera.position.set(-1, 0.5, 2.5);
+        camera.lookAt(0, 0.5);
+        close.visible = true;
+        cube.visible = true;
+        loadFont(text_third_floor, [-0.39, 0.6, 1.37], [0, -0.6, 0]);
+      }
+      if (intersects[i].object.name == 'canteen') {
+        textVisible.forEach((txt) => {
+          txt.visible = false;
+        });
+        intersects[i].object.material.color.set( 0x00ffff );
+        window.location.hash = "canteen";
+        close2.visible = true;
+        cube2.visible = true;
+        loadFont(text_canteen, [-2.1, 0.85, -1.68], [0, 0, 0]);
+      }
+      if (intersects[i].object.name == 'close') {
+        intersects[i].object.material.color.set( 0xffff00 );
+        cube.visible = false;
+        close.visible = false;
+        window.location.hash = "";
+        textVisible.forEach((txt) => {
+          txt.visible = false;
+        });
+        camera.position.set(0, 1, 4);
+        camera.lookAt(cameraLookAt);
+      }
+      if (intersects[i].object.name == 'close2') {
+        intersects[i].object.material.color.set( 0xffff00 );
+        cube2.visible = false;
+        close2.visible = false;
+        window.location.hash = "";
+        textVisible.forEach((txt) => {
+          txt.visible = false;
+        });
+        camera.position.set(0, 1, 4);
+        camera.lookAt(cameraLookAt);
+      }
+    }
+  }
+  
+  window.addEventListener( 'mousemove', onMouseMove, false );
+  window.addEventListener( 'click', onMouseClick, false );
+
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.shadowMap.enabled = true;
@@ -446,7 +628,6 @@ function init() {
   fogtoggler.addEventListener('click', toggleFog);
 
 }
-
 
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
